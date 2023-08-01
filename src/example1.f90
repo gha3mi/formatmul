f62ccc33853a2aee25a2c399539a29c03102e637
gha3mi <36126354+gha3mi@users.noreply.github.com>
2023-08-01 15:24:00 +0000

    Deploying to gh-pages from @ gha3mi/formatmul@436d50f53b55b29a7ccb931987a337d478ba5c59 🚀

---
+++ src/example1.f90
@@ -0,0 +1,30 @@
+program example1
+
+   use kinds,     only: rk
+   use fortime,   only: timer
+   use formatmul, only: matmul
+
+   implicit none
+
+   real(rk), allocatable :: A(:,:), B(:,:)
+   real(rk), allocatable :: C(:,:)
+   type(timer)           :: t
+   integer               :: m, n, o
+
+   ! C(m,o) = A(m,n).B(n,o)
+   m = 64*2*100
+   n = 16*100
+   o = 64*10
+
+   allocate(A(m,n),B(n,o))
+   call random_number(A)
+   call random_number(B)
+
+   sync all
+
+   if (this_image() == 1) call t%timer_start()
+      C = matmul(A,B)
+   if (this_image() == 1) call t%timer_stop(message=' Elapsed time (example1: mat_mat):')
+
+end program example1
+
