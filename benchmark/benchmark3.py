1b1ad6bc8adc2e7ca1c49db5ab329e668f2f8d54
Seyed Ali Ghasemi <info@gha3mi.com>
2023-12-14 19:02:39 +0100

    Add benchmark3.py for non-coarray.

--- benchmark/benchmark3.py
+++ benchmark/benchmark3.py
@@ -1,136 +1,65 @@
 # Formatmul
 # Seyed Ali Ghasemi
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from matplotlib.ticker import ScalarFormatter
-from itertools import cycle
-from collections import defaultdict
-import numpy as np
 
-# Read benchmark data from a file
-data = []
-with open('benchmark/benchmark3_im1.data', 'r') as file:
-    for _ in range(3):
-        next(file)  # Skip the first 3 rows
-    constant_value_row = next(file).strip().split()  # Read the constant value row
-    number_images = int(constant_value_row[1])  # Extract the number of images
-    for line in file:
-        values = line.strip().split()
-        identifier = values[0]
-        x_value = int(values[1])*int(values[3])  # m*o
-        elapsed_time = float(values[7])  # Extract elapsed time (y-value for elapsed time plot)
-        performance = float(values[8])  # Extract performance (y-value for performance plot)
-        data.append((identifier, x_value, elapsed_time, performance))
+# Define the file name as a variable
+file_name = 'benchmark/benchmark3.data'
 
-# Organize data based on the identifier
-organized_data_elapsed_time = defaultdict(list)
-organized_data_performance = defaultdict(list)
-for row in data:
-    identifier = row[0]
-    organized_data_elapsed_time[identifier].append((row[1], row[2]))
-    organized_data_performance[identifier].append((row[1], row[3]))
+# Read the benchmark data from the file
+benchmark_data = pd.read_csv(file_name, sep=' ', skiprows=3, header=None)
 
-# Create a figure with two subplots
-fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+# Assign column names to the benchmark data
+benchmark_data.columns = ['method', 'm', 'n', 'o', 'nloops', 'elapsed_time', 'gflops']
 
-# Assign unique markers and colors for each identifier
-identifier_marker_color = {}
-marker_cycle = cycle(['o','.', 'o', 's', '^', 'v', 'D', 'p', '*', '+', 'x', '<', '>','|'])
-color_cycle = cycle(['teal','black', 'blue', 'green', 'red', 'purple', 'orange', 'brown', 'pink', 'gray', 'gold', 'magenta', 'olive','maroon'])
+# Calculate the number of elements in the resulting matrix
+benchmark_data['num_elements'] = benchmark_data['m'] * benchmark_data['n'] * benchmark_data['o']
 
-# Plot elapsed time data
-for key, values in organized_data_elapsed_time.items():
-    x_values = [v[0] for v in values]
-    elapsed_time_values = [v[1] for v in values]
-    marker = next(marker_cycle)
-    color = next(color_cycle)
-    identifier_marker_color[key] = (marker, color)
+# Get unique methods in the dataset for plotting
+unique_methods = benchmark_data['method'].unique()
 
-    axes[0].plot(x_values, elapsed_time_values, marker=marker, color=color, linestyle='-', label=key)
+# Define markers and colors automatically for each unique method
+markers = {method: plt.Line2D.filled_markers[idx % len(plt.Line2D.filled_markers)]
+           for idx, method in enumerate(unique_methods)}
 
-# Configure and show the first subplot
-axes[0].set_title(f'Coarray Matmul Benchmark - Average Elapsed Time\nnumber_images={number_images}')
-axes[0].set_xlabel('Number of Elements in the Resulting Matrix')
-axes[0].set_ylabel('Average Elapsed Time [s]')
-axes[0].legend(loc='upper left', fontsize='small')
-axes[0].grid(True)
-axes[0].xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-axes[0].ticklabel_format(axis='x', style='sci', scilimits=(-2,2))
+# Generate colors based on the number of unique methods
+num_unique_methods = len(unique_methods)
+cmap = plt.cm.get_cmap('prism', num_unique_methods)  # Using 'tab20' colormap with the number of unique methods
+colors = {method: mpl.colors.rgb2hex(cmap(i)[:3]) for i, method in enumerate(unique_methods)}
 
-# Plot performance data
-for key, values in organized_data_performance.items():
-    x_values = [v[0] for v in values]
-    performance_values = [v[1] for v in values]
-    
-    marker, color = identifier_marker_color[key]
-    
-    axes[1].plot(x_values, performance_values, marker=marker, color=color, linestyle='-', label=key)
+# Create the first plot for elapsed_time vs size
+plt.figure(figsize=(7, 6))  # Create a new figure for the first plot
+plt.title(f'Matmul Benchmark - Average Elapsed Time')  # Title for the plot
+for method, group in benchmark_data.groupby('method'):
+    plt.plot(group['num_elements'], group['elapsed_time'], label=method, marker=markers[method], color=colors[method])
+plt.xlabel('Number of Elements in the Resulting Matrix')  # X-axis label
+plt.ylabel('Elapsed Time [s]')  # Y-axis label
+plt.legend(loc='upper left', bbox_to_anchor=(1, 1), ncol=1)  # Legend settings
+plt.gca().xaxis.set_major_formatter(ScalarFormatter(useMathText=True))  # X-axis format settings
+plt.gca().ticklabel_format(axis='x', style='sci', scilimits=(-2, 2))  # X-axis tick label format
+plt.grid(True)  # Enable grid
+plt.tight_layout()  # Adjust layout for better visualization
 
-# Configure and show the second subplot
-axes[1].set_title(f'Coarray Matmul Benchmark - Total Performance\nnumber_images={number_images}')
-axes[1].set_xlabel('Number of Elements in the Resulting Matrix')
-axes[1].set_ylabel('Total Performance [GFLOPS]')
-axes[1].legend(loc='upper left', fontsize='small')
-axes[1].grid(True)
-axes[1].xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-axes[1].ticklabel_format(axis='x', style='sci', scilimits=(-2,2))
+# Save the first plot as an image file
+plt.savefig(f'benchmark/benchmark3t.png', dpi=600)  # Save the plot as an image
 
-# Adjust layout, save the first set of plots, and display
-plt.tight_layout()
-plt.savefig(f'benchmark/benchmark3a_nim{number_images}.png', dpi=300)
-plt.show()
+# Create the second plot for gflops vs size
+plt.figure(figsize=(7, 6))  # Create another new figure for the second plot
+plt.title(f'Matmul Benchmark - Total Performance')  # Title for the plot
+for method, group in benchmark_data.groupby('method'):
+    plt.plot(group['num_elements'], group['gflops'], label=method, marker=markers[method], color=colors[method])
+plt.xlabel('Number of Elements in the Resulting Matrix')  # X-axis label
+plt.ylabel('Performance [GFLOPS]')  # Y-axis label
+plt.legend(loc='upper left', bbox_to_anchor=(1, 1), ncol=1)  # Legend settings
+plt.gca().xaxis.set_major_formatter(ScalarFormatter(useMathText=True))  # X-axis format settings
+plt.gca().ticklabel_format(axis='x', style='sci', scilimits=(-2, 2))  # X-axis tick label format
+plt.grid(True)  # Enable grid
+plt.tight_layout()  # Adjust layout for better visualization
 
-# Calculate average values for elapsed time and performance
-average_values_elapsed_time = {}
-average_values_performance = {}
-for key, values in organized_data_elapsed_time.items():
-    elapsed_time_values = [v[1] for v in values]
-    average_elapsed_time = np.mean(elapsed_time_values)
-    average_values_elapsed_time[key] = average_elapsed_time
-    
-for key, values in organized_data_performance.items():
-    performance_values = [v[1] for v in values]
-    average_performance = np.mean(performance_values)
-    average_values_performance[key] = average_performance
+# Save the second plot as an image file
+plt.savefig(f'benchmark/benchmark3p.png', dpi=600)  # Save the plot as an image
 
-# Find methods with the highest and lowest average elapsed time and performance
-max_elapsed_time_method = max(average_values_elapsed_time, key=average_values_elapsed_time.get)
-min_elapsed_time_method = min(average_values_elapsed_time, key=average_values_elapsed_time.get)
-
-max_performance_method = max(average_values_performance, key=average_values_performance.get)
-min_performance_method = min(average_values_performance, key=average_values_performance.get)
-
-# Calculate overall average values for elapsed time and performance
-overall_average_elapsed_time = np.mean(list(average_values_elapsed_time.values()))
-overall_average_performance = np.mean(list(average_values_performance.values()))
-
-# Create bar plots for average values
-fig, axes = plt.subplots(1, 2, figsize=(15, 6))
-
-# Bar plot for average elapsed time
-bar_colors_elapsed_time = ['green' if method == min_elapsed_time_method else 'red' if method == max_elapsed_time_method else 'blue' for method in average_values_elapsed_time.keys()]
-bars = axes[0].bar(average_values_elapsed_time.keys(), average_values_elapsed_time.values(), color=bar_colors_elapsed_time)
-
-# Add overall average value to the bar plot
-axes[0].axhline(y=overall_average_elapsed_time, color='gray', linestyle='dashed', label='Overall Average')
-axes[0].set_title(f'Coarray Matmul Benchmark - Average Elapsed Time\nnumber_images={number_images}')
-axes[0].set_xlabel('Methods')
-axes[0].set_ylabel('Average Elapsed Time [s]')
-axes[0].tick_params(axis='x', rotation=45)
-axes[0].grid(True)
-
-# Bar plot for average performance
-bar_colors_performance = ['green' if method == max_performance_method else 'red' if method == min_performance_method else 'blue' for method in average_values_performance.keys()]
-bars = axes[1].bar(average_values_performance.keys(), average_values_performance.values(), color=bar_colors_performance)
-
-# Add overall average value to the bar plot
-axes[1].axhline(y=overall_average_performance, color='gray', linestyle='dashed', label='Overall Average')
-axes[1].set_title(f'Coarray Matmul Benchmark - Average Total Performance\nnumber_images={number_images}')
-axes[1].set_xlabel('Methods')
-axes[1].set_ylabel('Average Total Performance [GFLOPS]')
-axes[1].tick_params(axis='x', rotation=45)
-axes[1].grid(True)
-
-# Adjust layout, save the second set of plots, and display
-plt.tight_layout()
-plt.savefig(f'benchmark/benchmark3b_nim{number_images}.png', dpi=300)
+# Display both plots
 plt.show()
